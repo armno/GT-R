79cd1d98729f9c258100cfb26d6546cec006a0cc
ArmNo <arm@snapplabs.com>
2013-03-26 16:18:51 +0700

    removed all whitespace shit

--- me.sh
+++ me.sh
@@ -10,14 +10,14 @@ clear
 
 echo "Aloha ArmNo! You re-install Ubuntu again!"
 echo ""
-echo "These are things I will do for you"
-echo ""
-echo "1. Update all packages"
-echo "2. Install Git and Vim"
-echo "3. Make Vim your default editor"
-echo "4. Install zsh"
-echo "5. Install your zsh theme (wow!)"
-echo ""
+#echo "These are things I will do for you"
+#echo ""
+#echo "1. Update all packages"
+#echo "2. Install Git and Vim"
+#echo "3. Make Vim your default editor"
+#echo "4. Install zsh"
+#echo "5. Install your zsh theme (wow!)"
+#echo ""
 echo "If you are ready, press Enter."
 read e
 
@@ -32,25 +32,20 @@ else
 fi
 
 # 1. Update all packages
-echo ""
 echo "Updating your packages ..."
 sudo apt-get update >> $logFile && sudo apt-get upgrade >> $logFile
-echo ""
 echo "All packages are up-to-date. You might need to restart your computer and run this script again :P"
 echo "Next step: Installing Git and Vim ..."
 
 # 2. Install Git and Vim
-echo ""
 echo "Installing your favorite tools: Git and Vim"
 sudo apt-get install git vim >> $logFile
-echo ""
 echo "Installed Git and Vim."
 
 # 3. Make Vim default editor
-echo ""
 echo "I know you hate Nano. Let's make Vim default."
+echo "SELECT NUMBER 3, THEN YOUR LIFE WILL BE EASIER"
 select-editor
-echo ""
 
 # 4. Make and ugly Vim more beautiful
 # should skip this step if there are already Lucius and .vimrc installed
@@ -67,20 +62,17 @@ else
 	echo "lucius.vim is downloaded at $luciusPath"
 fi
 
-echo ""
 if [ -f $vimRcPath ]
 then
   echo "Your .vimrc is already existed. Skipping fetching the file ..."
 else
   wget -q --no-check-certificate https://raw.github.com/armno/dotfiles/master/.vimrc -O $vimRcPath
-  source $vimRcPath >> $logFile
+  #source $vimRcPath >> $logFile
 fi
 
-echo ""
 echo "Your Vim looks fantastic with .vimrc and Lucius theme"
 
 # 4. Installing Zsh
-echo ""
 echo "I will make your terminal AWESOME with Zsh."
 
 # check if zsh is already installed
@@ -88,20 +80,16 @@ if type zsh >> $logFile
 	then
 	echo "Zsh is already installed on your machine. Boo!"
 else
-	echo ""
 	echo "Installing oh-my-zsh ..."
-	echo ""
 	sudo apt-get install zsh >> $logFile
 	wget -q --no-check-certificate https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O -- | sh >> $logFile
 	
 # make zsh defaul shell. forget about bash
-	echo ""
 	echo "Making Zsh the default shell ..."
-	echo ""
 	chsh -s `which zsh` >> $logFile
-	echo ""
 	echo "Zsh is now your default shell ..."
-	echo ""
 fi
+
+echo "I think that's it for now. See you soon!"
 
 exit 0
